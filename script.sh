5f5a30cf0f20f59c4ad5c210d902c0404ae238bd
Syed Hisham Akmal <hisham0502@gmail.com>
2024-01-03 12:50:33 +0530

    Enhanced logic and added .gitignore

--- script.sh
+++ script.sh
@@ -9,7 +9,13 @@
 
 # List all s3 buckets
 echo "List of s3 buckets"
-aws s3 ls
+s3_buckets=$(aws s3 ls)
+
+if [ -z "$s3_buckets" ]; then
+    echo "Err: No existing S3 buckets found"
+else
+    echo "$s3_buckets"
+fi 
 
 echo -e "\n-----------------------------------------------\n"
 
@@ -27,6 +33,7 @@ echo -e "\n-----------------------------------------------\n"
 
 # List all IAM users
 echo "List of IAM users"
-aws iam list-users
+# aws iam list-users | jq -r '.Users[] | "\(.UserName) \(.UserId)"' # Raw text format
+aws iam list-users | jq -r '[.Users[] | {UserName, UserId}]' # JSON format
 
 echo -e "\n-----------------------------------------------\n"
