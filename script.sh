e0b65e9a730836de8a202a28e24cab37270c2054
Syed Hisham Akmal <hisham0502@gmail.com>
2024-01-03 12:39:52 +0530

    Enhanced the representation of data

--- script.sh
+++ script.sh
@@ -4,20 +4,29 @@
 
 # Resources covered: AWS S3, AWS EC2, AWS Lambda and AWS IAM
 
+# Enable debugging mode
+# set -x
+
 # List all s3 buckets
 echo "List of s3 buckets"
 aws s3 ls
+
+echo -e "\n-----------------------------------------------\n"
 
 #  List all ec2 instances
 echo "List of ec2 instances"
 aws ec2 describe-instances
 
+echo -e "\n-----------------------------------------------\n"
+
 # List all lambda functions
 echo "List of lambda functions"
 aws lambda list-functions
 
-echo "\n-----------------------------------------------\n"
+echo -e "\n-----------------------------------------------\n"
 
 # List all IAM users
 echo "List of IAM users"
 aws iam list-users
+
+echo -e "\n-----------------------------------------------\n"
